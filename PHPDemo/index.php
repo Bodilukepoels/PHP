ac715a213d9b4052f03c0bca24eac03c99a98988
Bodilukepoels <112855865+Bodilukepoels@users.noreply.github.com>
2023-01-09 10:13:44 +0100

    Heel hoofdstuk 2

---
+++ PHPDemo/index.php
@@ -0,0 +1,29 @@
+<?php
+$a = "Ik heb niet gefaald.";
+$b = "die niet werken.";
+$c = "10000";
+
+echo $a . " Ik heb alleen " . $c . " manieren gevonden" . $b;
+echo "<br>";
+
+$a2 = "die geen fout";
+$b2 = "en";
+$c2 = "niet.";
+
+echo " Mensen " . $a2 . $b2 . "," . " werken " . $c2;
+echo "<br>";
+
+$A = "ry";
+$B = ", but";
+$C = "fail";
+
+echo "T" . $A . " and " . $C . $B . " never " . $C . " to " . "t" . $A;
+echo "<br>";
+
+$A2 = "\"omgaan";
+$B2 = "met";
+$C2 = "teleu";
+
+echo "De cursus " . $A2 . " " . $B2 . " " . $C2 . "rstellingen\"" . " kan vanavond helaas niet doorgaan."
+
+?>
